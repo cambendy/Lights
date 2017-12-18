71612a5c5748a769c863566a3949dc600228ec2c
CamBendy <cambendy@users.noreply.github.com>
2017-12-18 11:15:08 +1100

    Added new mode to iterate through all modes, changing after 2 minutes

--- src/Lights.ino
+++ src/Lights.ino
@@ -85,10 +85,10 @@ void setup()
 // List of patterns to cycle through.  Each is defined as a separate function below.
 typedef void (*ModeList[])();
 
-ModeList gModes = { blueLights,  blueWithGlitter, oneColor, rainbow, rainbowWithGlitter, confetti, blueRedLights} ;
-const char *Modes[] = { "blueLights", "blueWithGlitter", "oneColor", "rainbow", "rainbowWithGlitter", "confetti", "blueRedLights"};
+ModeList gModes = { blueLights,  blueWithGlitter, oneColor, rainbow, rainbowWithGlitter, confetti, blueRedLights, justGlitter, rotateAllModes} ;
+const char *Modes[] = { "blueLights", "blueWithGlitter", "oneColor", "rainbow", "rainbowWithGlitter", "confetti", "blueRedLights", "justGlitter", "rotateAllModes"};
 
-uint8_t gCurrentMode = 0; // Index number of which mode is current
+uint8_t gCurrentMode = (ARRAY_SIZE(gModes) - 1); // Index number of which mode is current
 
 void loop()
 {
@@ -238,39 +238,6 @@ void randomConfetti(CRGB * liteArray, int ledCount)
   }
 }
 
-
-/* void sinelon()
-{
-  // a colored dot sweeping back and forth, with fading trails
-  fadeNow(20);
-  int pos = beatsin16(13, 0, NUM_SETTINGS_LEDS - 1);
-  settingsLEDS[pos] += CHSV(gHue, 255, 192);
-}
-
-void bpm()
-{
-  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
-  uint8_t BeatsPerMinute = 62;
-  CRGBPalette16 palette = PartyColors_p;
-  uint8_t beat = beatsin8(BeatsPerMinute, 64, 255);
-  for (int i = 0; i < NUM_SETTINGS_LEDS; i++)
-  { //9948
-    settingsLEDS[i] = ColorFromPalette(palette, gHue + (i * 2), beat - gHue + (i * 10));
-  }
-}
-
-void juggle()
-{
-  // eight colored dots, weaving in and out of sync with each other
-  fadeNow(20);
-  byte dothue = 0;
-  for (int i = 0; i < 8; i++)
-  {
-    settingsLEDS[beatsin16(i + 7, 0, NUM_SETTINGS_LEDS - 1)] |= CHSV(dothue, 200, 255);
-    dothue += 32;
-  }
-}
- */
 bool isFade = false;
 int fadeVal = 25;
 int randVal = 40;
@@ -416,4 +383,39 @@ extern const TProgmemRGBPalette32 BlueRedColors_p FL_PROGMEM =
 CRGB aRandomBlueRed()
 {
    return ColorFromPalette( BlueRedColors_p, random8(31) );
+}
+
+void justGlitter()
+{
+  fadeNow(90);
+  addGlitter(50);
+}
+
+uint8_t gTempCurrentMode = 0; // Index number of which mode is current
+unsigned long tempModeTime = 0; // Used to time out MODE changing
+unsigned long tempModeLength = 120000; //How long to let each mode run for
+
+void rotateAllModes()
+{
+/*     Serial.print("Temp time = ");
+    Serial.print(tempModeTime + tempModeLength);
+    Serial.print("Temp mode = ");
+    Serial.print(tempModeTime );
+    Serial.print("Temp mode legnth= ");
+    Serial.print( tempModeLength);
+    Serial.print(" - Milli = ");
+    Serial.println(millis());
+ */  
+  if (tempModeTime + tempModeLength < millis()) {
+    //change modes
+    gTempCurrentMode = (gTempCurrentMode + 1) % (ARRAY_SIZE(gModes) - 1);
+    Serial.print("Temp Mode = ");
+    Serial.print(gTempCurrentMode);
+    Serial.print(" - ");
+    Serial.println(Modes[gTempCurrentMode]);
+    tempModeTime = millis();
+  }
+  
+  //run the temp mode pattern 
+  gModes[gTempCurrentMode](); 
 }
